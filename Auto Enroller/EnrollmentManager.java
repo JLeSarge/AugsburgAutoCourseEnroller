7598e9e8925dd5643bc40c19c6bf34b2653e8d52
aisha0j <159175861+aisha0j@users.noreply.github.com>
2024-04-11 11:12:50 -0500

    enrollmentCODE
    code

---
+++ Auto Enroller/EnrollmentManager.java
@@ -0,0 +1,71 @@
+import java.util.List;
+import java.util.ArrayList;
+public class EnrollmentManager {
+    private List<Course> availableCourses;
+    public class Student {
+        private String name;
+    
+        public Student(String name) {
+            this.name = name;
+        }
+    
+        public String getName() {
+            return name;
+        }
+    
+    }
+    
+    public class Course {
+        private String courseName;
+    
+        public Course(String courseName) {
+            this.courseName = courseName;
+        }
+    
+        public String getCourseName() {
+            return courseName;
+        }
+    
+    }
+
+    public EnrollmentManager(List<Course> availableCourses) {
+        this.availableCourses = availableCourses;
+    }
+
+    public void enrollStudent(Student student, Course course) {
+        // check if the course is available for enrollment
+        if (availableCourses.contains(course)) {
+            // check eligibility based on enrollement rule
+            EnrollmentRule enrollmentRule = course.getEnrollmentRule();
+            if (enrollmentRule != null && enrollmentRule.isEligible(student, course)) {
+                // check if the course has reached capacity
+                if (course.getEnrolledStudents().size() < course.getCapacity()) {
+                    // enroll student
+                    course.enrollStudent(student);
+                    System.out.println("Student" + student.getName() + " enrolled in course " + course.getCourseName());
+                } else {
+                    System.out.println("Course " + course.getCourseName() + " is already full");
+                } 
+                 } else {
+                    System.out.println("Student" + student.getName() + " is not eligible to enroll in course " + course.getCourseName());
+                }
+            }else {
+                System.out.println("Course" + course.getCourseName() + " is not availible for enrollment." );
+            }
+              
+    }
+    
+
+    public List<Course> getAvailableCourses() {
+        List<Course> available = new ArrayList<>();
+        for (Course course : availableCourses) {
+            if (course.getEnrollmentRule() != null && course.getEnrollmentRule().isWithinEnrollmentPeriod()) {
+                available.add(course);
+            }
+        }
+        // return available courses
+        // retrieves courses that are available for enrollment
+        return available;
+    }
+    
+}
