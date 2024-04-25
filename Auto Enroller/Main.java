c0bd32b828ccb8afab44569f29ad710d82b51dac
JLeSarge <jarlesarge@gmail.com>
2024-04-24 22:33:07 -0500

    added sql stuff

--- Auto Enroller/Main.java
+++ Auto Enroller/Main.java
@@ -1,6 +1,12 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
@@ -89,7 +95,7 @@ public class Main {
         }
     }
 
-    public static class EnrollmentManager {
+    public class EnrollmentManager {
         private List<Courses> availableCourses;
     
         public EnrollmentManager(List<Courses> availableCourses) {
@@ -106,17 +112,22 @@ public class Main {
                     if (course.getEnrolledStudents().size() < course.getCapacity()) {
                         // enroll student
                         course.enrollStudent(student);
+                        student.setEnrolled(true);
                         System.out.println("Student" + student.getName() + " enrolled in course " + course.getCourseName());
                     } else {
+                        student.setEnrolled(false);
                         System.out.println("Course " + course.getCourseName() + " is already full");
                     } 
                      } else {
+                        student.setEnrolled(false);
                         System.out.println("Student" + student.getName() + " is not eligible to enroll in course " + course.getCourseName());
                     }
                 }else {
+                    student.setEnrolled(false);
                     System.out.println("Course" + course.getCourseName() + " is not availible for enrollment." );
                 }
-                  
+                insertIntoDatabase(student);
+                
         }
         
     
@@ -130,6 +141,12 @@ public class Main {
             // returns available courses
             // retrieves courses that are available for enrollment
             return available;
+        }
+        // method that calls database connector method to update db
+        public static void insertIntoDatabase(Student stu){
+            // connect to database and prepare to insert student info
+            DatabaseConnector dbc = new DatabaseConnector();
+            dbc.connectAndUpdate(stu);  
         }
     
     }
@@ -174,6 +191,85 @@ public class Main {
             return currentTime.isAfter(enrollmentStartTime) && currentTime.isBefore(enrollmentEndTime);
         }
     
+    }
+
+    public class DatabaseConnector {
+        private static String  URL = "jdbc:mysql://Localhost:3307/?user=root&password=password";
+        private static Student  student;
+        private static Connection myConn = null;
+        private static Statement myStat;
+        private static LocalDate event;
+    
+        public static void setUSER(Student stu){
+            student = stu;
+        }
+        public static void createStatment(){
+            try {
+            myStat = myConn.createStatement();
+            } 
+            catch (SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+        public static void createConnectionInstance(){
+            try{
+                myConn = DriverManager.getConnection(URL);
+            }
+            catch(SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+        public static void createEventTime(){
+            event = LocalDate.now();
+        }
+        //the actual sql statment that inserts the information (name,studentid,date)
+        public static String createSQL(){
+            int enrolled = 0;
+            if (student.getEnrolled()){enrolled = 1;}
+            System.out.println("insert into `basic-database`.student_info (name, augsburg_id, event_log, Enrollment_Condition) values ('"
+                +student.getName()+"', '"+student.getID()+"', '"+event+"', '"+enrolled+"');");
+            return
+                    ("insert into `basic-database`.student_info (name, augsburg_id, event_log, Enrollment_Condition) values ('"
+                +student.getName()+"', '"+student.getID()+"', '"+event+"', '"+enrolled+"');");
+        }
+        //uses mystat to create an update statment for sql
+        public static void executeSQL(){
+            try{
+            myStat.executeUpdate(createSQL());
+            }
+            catch(SQLException e){
+                e.printStackTrace();
+            }
+        }
+        //combines other methods to generate all the variables in one method.
+        public static void initalizeVariables(Student student){
+            setUSER(student);
+            createStatment();
+            createEventTime();
+        }
+        //runs connect to database instance, intialize variables and executes the sql update
+        public static void connectAndUpdate(Student stu){
+            
+            try{
+                loadDriver();
+                createConnectionInstance();
+                initalizeVariables(stu);
+                executeSQL();
+            }
+            catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
+        // initalize driver
+        public static void loadDriver(){
+            try{
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
