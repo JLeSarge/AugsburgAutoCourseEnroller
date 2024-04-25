fff07b28bc292055ac6529ed054822892c248aca
v0ncent <83373495+v0ncent@users.noreply.github.com>
2024-04-25 00:19:56 -0500

    bruh

--- userinputhandler.js
+++ userinputhandler.js
@@ -7,15 +7,15 @@ document.addEventListener('DOMContentLoaded', function () {
         const augsburgURL = 'https://augnet.augsburg.edu/';
 
         // Get the username and password entered by the user
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
+        // const username = document.getElementById('username').value;
+        // const password = document.getElementById('password').value;
 
-        // Send the username and password to the content script to handle login
-        chrome.runtime.sendMessage({
-            action: 'login',
-            username: username,
-            password: password
-        });
+        // // Send the username and password to the content script to handle login
+        // chrome.runtime.sendMessage({
+        //     action: 'login',
+        //     username: username,
+        //     password: password
+        // });
 
         // Open a new tab with the Augsburg web page
         chrome.tabs.create({
