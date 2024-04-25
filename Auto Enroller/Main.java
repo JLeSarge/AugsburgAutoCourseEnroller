9c3dc0b4914738948c9e618d112a50785a198c58
JLeSarge <jarlesarge@gmail.com>
2024-04-24 22:59:46 -0500

    remove unused imports

--- Auto Enroller/Main.java
+++ Auto Enroller/Main.java
@@ -6,8 +6,6 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
